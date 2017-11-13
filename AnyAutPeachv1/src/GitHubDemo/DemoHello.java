2606928be580b39ced85aa5fed90ddf9597d343d
Archana <singh.archana2405@gmail.com>
2017-11-12 20:42:05 -0800

    Added comment

--- AnyAutPeachv1/src/GitHubDemo/DemoHello.java
+++ AnyAutPeachv1/src/GitHubDemo/DemoHello.java
@@ -5,7 +5,7 @@ public class DemoHello {
 	public static void main(String[] args) {
 		String[] names={"Bob", "Sam","Mary"};
 		
-		//printing 
+		//printing  names of few more folks
 		for(int i=0;i<names.length;i++)
 		{
 			System.out.format("Hello,\n",names[i]);
