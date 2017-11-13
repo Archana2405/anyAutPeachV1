ed28c5097e41163d7d0f7c802b3b8a2044accfde
Archana 2 <singh.archana2405@gmail.com>
2017-11-13 13:05:10 -0800

    Commiting developer 2

--- AnyAutPeachv1/src/GitHubDemo/DemoHello.java
+++ AnyAutPeachv1/src/GitHubDemo/DemoHello.java
@@ -12,16 +12,11 @@ public class DemoHello {
 		
 		for(String name:names)
 		{
-<<<<<<< HEAD
-			
-			System.out.println("Names="+ names[name])
-			System.out.println("Second modification but not committed");
-=======
 
 			System.out.format("Hello,\n",names[i]);
 
 			System.out.println("Second modification and committed");
->>>>>>> branch 'archana' of https://github.com/Archana2405/anyAutPeachV1.git
+
 
 
 		//Committed code and pushing it
