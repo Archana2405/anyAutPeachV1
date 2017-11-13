a7d26a4b7931eeaf6b024647a072c16dc63d7c4c
Archana2 <singh.archana2405@gmail.com>
2017-11-13 10:37:31 -0800

    Removed names and added for loop

--- AnyAutPeachv1/src/GitHubDemo/DemoHello.java
+++ AnyAutPeachv1/src/GitHubDemo/DemoHello.java
@@ -4,15 +4,15 @@ public class DemoHello {
 
 	public static void main(String[] args) {
 
-		String[] names={"Bob", "Sam","Mary","Ruby","Sun", "Mon"};
+		String[] names={"Bob", "Sam","Mary"};
 
 		
 		//Workspace 2
 		
-		for(String name:names)
+		for(int i=0;i<names;i++)
 		{
 
-			System.out.format("Hello,\n",name);
+			System.out.format("Hello,\n",names[i]);
 
 
 		}
