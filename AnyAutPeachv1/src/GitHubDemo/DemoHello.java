693ef17cc432a95de7d2fb0a1df9947b80f46bd9
Archana2 <singh.archana2405@gmail.com>
2017-11-12 20:43:22 -0800

    Added names

--- AnyAutPeachv1/src/GitHubDemo/DemoHello.java
+++ AnyAutPeachv1/src/GitHubDemo/DemoHello.java
@@ -3,12 +3,13 @@ package GitHubDemo;
 public class DemoHello {
 
 	public static void main(String[] args) {
-		String[] names={"Bob", "Sam","Mary"};
+		String[] names={"Bob", "Sam","Mary","Singh"};
 		
 		//printing 
 		for(int i=0;i<names.length;i++)
 		{
 			System.out.format("Hello,\n",names[i]);
+			System.out.println("How are you?");
 		}
 
 	}
