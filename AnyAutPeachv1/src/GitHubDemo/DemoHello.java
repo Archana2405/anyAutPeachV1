0ce416a92cabf0f128efabdcdb1c6e4900b81483
Archana <singh.archana2405@gmail.com>
2017-11-12 19:57:08 -0800

    commit 1

---
+++ AnyAutPeachv1/src/GitHubDemo/DemoHello.java
@@ -0,0 +1,16 @@
+package GitHubDemo;
+
+public class DemoHello {
+
+	public static void main(String[] args) {
+		String[] names={"Bob", "Sam","Mary"};
+		
+		//printing 
+		for(int i=0;i<names.length;i++)
+		{
+			System.out.format("Hello,\n",names[i]);
+		}
+
+	}
+
+}
