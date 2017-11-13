5837afe92079df87bd01fc8142bea92b24c16bf9
Archana <singh.archana2405@gmail.com>
2017-11-12 20:51:36 -0800

    Added few more names

--- AnyAutPeachv1/src/GitHubDemo/DemoHello.java
+++ AnyAutPeachv1/src/GitHubDemo/DemoHello.java
@@ -3,7 +3,7 @@ package GitHubDemo;
 public class DemoHello {
 
 	public static void main(String[] args) {
-		String[] names={"Bob", "Sam","Mary"};
+		String[] names={"Bob", "Sam","Mary", "James", "Jay"};
 		
 		//printing  names of few more folks
 		for(int i=0;i<names.length;i++)
