531c372a6d8682f1ae257d920307f3890885a9c4
Archana <singh.archana2405@gmail.com>
2017-11-14 11:54:54 -0800

    Adding For each

--- AnyAutPeachv1/src/GitHubDemo/DemoHello.java
+++ AnyAutPeachv1/src/GitHubDemo/DemoHello.java
@@ -10,10 +10,10 @@ public class DemoHello {
 				
 		//Workspace 2
 		
-		for(int i=0;i<names[i];i++)
+		for(String name:names)
 		{
 
-			System.out.format("Hello,\n",names[i]);
+			System.out.format("Hello,\n",name);
 
 
 	}
